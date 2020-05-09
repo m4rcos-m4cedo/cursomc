f706d8f3596fa4baa05951706c9591037364f4ce
Marcos Macedo <ricardo.paiva.virtual@gmail.com>
2020-05-09 19:12:28 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/br/com/mundovirtual/services/CategoriaService.java
+++ src/main/java/br/com/mundovirtual/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.mundovirtual.domain.Categoria;
 import br.com.mundovirtual.repositories.CategoriaRepository;
+import br.com.mundovirtual.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,11 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
+		
+		if (!obj.isPresent()) {
+			throw new ObjectNotFoundException("Objeto não encontrado!");
+		}
+		
 		return obj.orElse(null);
 	}
 
